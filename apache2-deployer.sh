a55b28ee95be5e46c815d244acdcb17310822228
UltimateByte <robin.labadie@gmail.com>
2016-09-05 01:15:47 +0200

    Polish

--- apache2-deployer.sh
+++ apache2-deployer.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
+# Apache2 website deployment
 # Authors: UltimateByte, BassSpleen
+# Website: https://www.terageek.org
 # Description: Creates required files and folders to deploy an apache2 website in one command
  
-# Settings Variables
+### SETTINGS ##
+# The parent folder for your user's home directory (usually /home)
 homedir="/home"
+# The name of you website's folder (usually public_html or www)
 webdir="public_html"
 
-###########
-# Program #
-###########
+#############
+## Program ##
+#############
 
 # Misc Variables
 selfname="$(basename "$(readlink -f "${BASH_SOURCE[0]}")")"
 apache_sites="/etc/apache2/sites-available"
+apacheprocess="www-data"
+
+# Check that the user inputted two arguments
+if [ -z "$1" ] || [ -z "$2" ]; then
+	echo "Info! Please, specify a username and a domain for the website"
+	echo "Example: ./${selfname} username domain.com"
+	sleep 1
+	exit 1
+elif [ -n "$3" ]; then
+	echo "[ERROR] Too many arguments!"
+	sleep 1
+	echo "Info! Please specify a username and a domain for the website"
+	echo "Example: ./${selfname} username domain.com"
+	exit 1
+else
+	# Use a variable name that makes sense
+	username="${1}"
+	# Strip www. from user input since we're adding it as an alias
+	domain="${2#www.}"
+fi
 
 # Check that the script is launched with elevated privileges
 fn_check_root(){
 	if [ "$(id -u)" != "0" ]; then
-		echo "This script must be run with elevated privileges"
+		echo "[ERROR] This script must be run with elevated privileges"
 		exit 1
 	fi
 }
@@ -31,17 +55,16 @@ fn_check_vars(){
 		check_homedir=0
 	else
 		check_homedir=1
-		echo "Could not find ${homedir}"
+		echo "[ERROR] Could not find ${homedir}"
 		echo "Please, set a valid homedir value"
 		sleep 2
 	fi
- 
 	# Checking that the variable directory has been set
 	if [ -n "${webdir}" ]; then
 		check_webdir=0
 	else
 		check_webdir=1
-		echo "No webdir set"
+		echo "[ERROR] No webdir set"
 		echo "Please, set a valid webdir"
 	sleep 2
 	fi
@@ -51,62 +74,115 @@ fn_check_vars(){
 		check_apachedir=0
 	else
 		check_apachedir=1
-		echo "${apache_sites} folder not found"
-		echo "Please, install Apache2"
+		echo "[ERROR] Could not find ${apache_sites}"
+		echo "Please, install apache2"
 		sleep 2
 	fi
  
 	# Check summ up
 	if [ "${check_homedir}" == "1" ] || [ "${check_webdir}" == "1" ] || [ "${check_apachedir}" == "1" ]; then
-		echo "Exiting"
-		sleep 2
+		echo "Exiting..."
+		sleep 1
 		exit 1
 	else
-		echo "Config OK!"
+		echo ""
+		echo "[OK] Config test passed!"
+		sleep 1
+		echo ""
 		# Setting directories
 		userdir="${homedir}/${username}"
 		targetdir="${userdir}/${webdir}"
-		sleep 1
 	fi
+}
+
+fn_welcome_prompt(){
+	echo "########################################################"
+	echo "########## Apache 2 website deployment script ##########"
+	echo "########################################################"
+	sleep 2
+	echo ""
+	echo "Welcome!"
+	echo ""
+	echo "Here are your settings:"
+	echo "Domain: ${domain}"
+	echo "Username: ${username}"
+	echo "Target directory: ${targetdir}"
+	echo ""
+	while true; do
+		read -e -i "y" -p "Continue? [Y/n]" yn
+		case $yn in
+		[Yy]* ) echo "Let's go!"; sleep 1; break;;
+		[Nn]* ) echo "Maybe next time!"; return;;
+		* ) echo "Please answer yes or no.";;
+	esac
+	done
 }
 
 # Adding a user with the correct homedir
 fn_add_user(){
+	echo ""
+	echo "#################### User Creation ####################"
+	echo ""
+	sleep 2
 	echo "Creating ${username}..."
 	sleep 1
 	useradd -m -d "${userdir}" "${username}"
-	echo "Please, input a password for ${username}"
+	echo "[OK] User created!"
 	sleep 1
+	echo ""
+	echo "[PASSWORD] Please, input a password for ${username}"
 	passwd "${username}"
-	echo "Password set!"
+	echo "[OK] Password set!"
 	sleep 1
 }
 
 # Creating the web directory and applying permissions
 fn_web_directory(){
-	echo "Creating the web directory"
+	echo ""
+	echo "################## Directory Creation ##################"
+	echo ""
+	sleep 2
+	echo "Creating the web directory..."
 	sleep 1
 	mkdir -pv "${targetdir}"
-	echo "Applying correct ownership/permissions to the website folder..."
+	echo "[OK] Directory created!"
 	sleep 1
+	echo ""
+	echo "Applying correct ownership & permissions to the website folder..."
+	sleep 2
 	chown -R "${username}":"${username}" "${targetdir}"
 	chmod -R 770 "${targetdir}"
 	chmod -R g+s "${targetdir}"
-	echo "Adding ${username} group to www-data"
+	echo "[OK] Ownership & permissions set!"
 	sleep 1
-	usermod -a -G "${username}" www-data
+	echo ""
+	echo "Adding ${username} group to ${apacheprocess}..."
+	sleep 2
+	usermod -a -G "${username}" "${apacheprocess}"
+	echo "[OK] Added user group to ${apacheprocess}!"
+	sleep 1
+	echo ""
+	echo "Restarting apache2 to enable group modifications..."
+	sleep 1
+	service apache2 restart
+	echo "[OK] apache2 restarted!"
+	sleep 1
 }
 
 # Create the Virtual Host config file and enable the site in apache
-
 fn_create_vhosts(){
+	echo ""
+	echo "################# VirtualHosts Creation #################"
+	echo ""
+	sleep 2
+	echo "Generating config file..."
 	touch "${apache_sites}"/"${domain}".conf
-	
+	sleep 1
 	echo "<VirtualHost *:80>
 	# Addresses
 	ServerName ${domain}
 	ServerAlias www.${domain}
-	ServerAdmin admin@localhost
+	ServerAdmin admin@${domain}
 
 	# Directory and rules
 	DocumentRoot ${targetdir}
@@ -122,56 +198,49 @@ fn_create_vhosts(){
 	ErrorLog \${APACHE_LOG_DIR}/${domain}-error.log
 	CustomLog \${APACHE_LOG_DIR}/${domain}-access.log combined
 </VirtualHost>" >> "${apache_sites}"/"${domain}".conf
+	echo "[OK] Config file generated!"
+	sleep 1
 }
 
+# Enable the website and restart apache
 fn_ensite(){
-	echo "Enableing website ${domain}"
+	echo ""
+	echo "################## Enabling Website ##################"
+	echo ""
+	sleep 2
+	echo "Enabling config for ${domain}..."
 	sleep 1
 	a2ensite "${domain}".conf
-	echo "Reloading Apache2 config"
+	echo "[OK] Config enabled"
+	echo "Reloading apache2 to apply config..."
 	sleep 1
 	# Reloading apache to activate the new website
 	service apache2 reload
+	echo "[OK] apache2 reloaded"
+	sleep 1
 }
 
 fn_conclusion(){
+	
+	echo "########################################################"
+	echo "###################### Job Done  #######################"
+	echo "########################################################"
+	sleep 2
 	echo ""
-	echo "Job done!"
-	sleep 1
 	echo "Time to add your website into ${targetdir}"
 	echo "Time to make ${domain} point to this machine"
-	sleep 1
+	sleep 2
 	echo ""
-	echo "Credits:"
+	echo "###################### Credits  ########################"
 	echo " -Idea and base: BassSpleen"
-	echo " -Code rework: UltimateByte (terageek.org & gameservermanagers.com)"
-	echo""
-	sleep 1
-	echo "We wish you the best!"
+	echo " -Code overhaul: UltimateByte (terageek.org & gameservermanagers.com)"
+	echo "[OK] We wish you the best!"
 }
 
 # Starting functions
-
 fn_check_root
-
-# Check that the user inputted two arguments
-if [ -z "$1" ] || [ -z "$2" ]; then
-	echo "Please specify a username and a domain for the website"
-	echo "Example : ./${selfname} username domain.com"
-	exit 1
-elif [ -n "$3" ]; then
-	echo "Too many arguments!"
-	echo "Please specify a username and a domain for the website"
-	echo "Example : ./${selfname} username domain.com"
-	exit 1
-else
-	# Use a variable name that makes sense
-	username="${1}"
-	# Strip www. from user input since we're adding it as an alias
-	domain="${2#www.}"
-fi
-
 fn_check_vars
+fn_welcome_prompt
 fn_add_user
 fn_web_directory
 fn_create_vhosts
